351778ac04d867da120426af1c7c19d8f0a82f53
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-03-27 02:19:03 +0000

    gualdado

---
+++ main.py
@@ -0,0 +1,16 @@
+numeroACambiar = "192"
+
+punto = False
+entero = []
+decimal = []
+
+for char in numeroACambiar:
+
+    if punto == False:
+
+        entero.append(int(char));
+
+    
+print(entero)
+
+
